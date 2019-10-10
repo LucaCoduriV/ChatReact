944c1ac8920d4829c15abf03afd873f8160b606c
Luca.CODURI <coduri.vitor@gmail.com>
2019-10-10 15:04:39 +0200

    Chat fichier de base

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    message: ""
+  }
+
+  handleChatSubmit(event){
+
+  }
+
+  handleMessage(event){
+
+  }
+
+  render() {
+    return (
+        <div class="chat">
+          <form onSubmit={event => this.handleChatSubmit(event)}>
+            <label>Message:</label>
+            <input onChange={event => this.handleMessage(event)}/>
+          </form>
+        </div>
+    );
+  }
 }
 
 export default App;
